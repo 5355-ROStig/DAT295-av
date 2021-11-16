b72ecb4845b6ee73f9cef708f4e1ad183494ecbd
wjohnsson@gmail.com <wjohnsson@gmail.com>
2021-11-16 11:00:40 +0100

    Add mapdata package from 2020 bachelor's thesis

---
+++ src/mapdata/README.md
@@ -0,0 +1,8 @@
+Map Data Node 2020
+==================
+
+Hosts ROS services for providing other nodes with information about the map.
+This allows swapping out the map/road system that is used in the system.
+
+Depends on master2020 branch of custom_msgs repo for messages and service 
+descriptions.
---
+++ src/mapdata/scripts/map_data_node.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+A ROS node to provide other nodes with information about the static environment.
+
+Copyright (c) 2020, Thomas Alexandersson
+Copyright (c) 2020, Farzad Besharati
+Copyright (c) 2020, Johannes Gustavsson
+Copyright (c) 2020, Martin Hilgendorf
+Copyright (c) 2020, Ivan Lyesnukhin
+Copyright (c) 2020, Jian Shin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Chalmers University of Technology nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+import rospy
+
+from map_data_reader import load_map
+
+from environment import Map
+from nav_msgs.srv import GetMap
+from mapdata.srv import GetCDM, GetGraph
+
+
+class MapData:
+    def __init__(self):
+        rospy.init_node('map_data')
+
+        map_name = rospy.get_param("~map_name")
+
+        self.map_data: Map = load_map(map_name)
+        rospy.loginfo(f"Loaded map '{map_name}'")
+
+        rospy.loginfo("Starting map occupancy grid service")
+        map_service = rospy.Service('map_occupancy_grid', GetMap, self.map_occupancy_grid_handler)
+
+        rospy.loginfo("Starting map graph service")
+        graph_service = rospy.Service('map_graph', GetGraph, self.map_graph_handler)
+
+        rospy.loginfo("Starting CDM data service")
+        graph_service = rospy.Service('cdm_data', GetCDM, self.cdm_data_handler)
+
+    def map_occupancy_grid_handler(self, data):
+        return self.map_data.get_occupancy_grid()
+
+    def map_graph_handler(self, data):
+        return self.map_data.get_graph()
+
+    def cdm_data_handler(self, data):
+        return self.map_data.get_cdm_data()
+
+
+if __name__ == '__main__':
+    MapData()
+    rospy.spin()
