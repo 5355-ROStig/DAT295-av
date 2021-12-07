6db275470e70054811e7eca8f182496207a87cbf
Oskar Fredriksson <oggehej@hotmail.com>
2021-12-07 20:37:21 +0100

    rly?

--- src/coordination/src/coordination_strategies/__init__.py
+++ src/coordination/src/coordination_strategies/__init__.py
@@ -8,9 +8,9 @@ from coordination_strategies.stop_road import StopRoad
 from coordination_strategies.strategy import CoordinationStrategy
 from coordination_strategies.traffic_light import TrafficLight
 
-COORDINATION_STRATEGIES: Mapping[str, Type[CoordinationStrategy]] = {
-    RoadSection.PRIORITY_ROAD: GiveWayRoad,
-    RoadSection.GIVE_WAY: PriorityRoad,
+COORDINATION_STRATEGIES: Mapping[int, Type[CoordinationStrategy]] = {
+    RoadSection.PRIORITY_ROAD: PriorityRoad,
+    RoadSection.GIVE_WAY: GiveWayRoad,
     RoadSection.STOP_SIGN: StopRoad,
     RoadSection.TRAFFIC_LIGHT: TrafficLight
 }
