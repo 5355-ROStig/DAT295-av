c15c31aa2d26e7e6303bf0835c85ee70eccee357
Martin H <marhilg@student.chalmers.se>
2021-12-08 16:43:06 +0100

    coordination: Implement stop sign strategy

--- src/coordination/src/coordination_strategies/strategy.py
+++ src/coordination/src/coordination_strategies/strategy.py
@@ -15,6 +15,16 @@ class CoordinationStrategy(ABC):
     def name(self) -> str:
         return self.name
 
+    def _is_to_the_right(self, me: str, other: str) -> bool:
+        mapping = {
+            'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'
+        }
+        return mapping[me] == other
+
     @abstractmethod
     def has_priority(self) -> bool:
-        ...
+        assert self.coordinator.start_road is not None
+        assert self.coordinator.other_croad is not None
+
+        return self._is_to_the_right(me=self.coordinator.start_road,
+                                     other=self.coordinator.other_croad)
