1b888fa1760c89b51bbb9b28f74f0b5035538d05
Martin H <marhilg@student.chalmers.se>
2021-12-08 16:55:19 +0100

    coordination: Put stop strategy in stop strategy file

--- src/coordination/src/coordination_strategies/stop_road.py
+++ src/coordination/src/coordination_strategies/stop_road.py
@@ -1,9 +1,24 @@
 from coordination_strategies.strategy import CoordinationStrategy
 
+import rospy
+
 
 class StopRoad(CoordinationStrategy):
     def __init__(self, coordinator):
         super().__init__("Stop-sign", coordinator)
 
+    def _is_to_the_right(self, me: str, other: str) -> bool:
+        mapping = {
+            'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'
+        }
+        return mapping[me] == other
+
     def has_priority(self) -> bool:
-        ...
+        assert self.coordinator.start_road is not None
+        assert self.coordinator.other_croad is not None
+
+        rospy.loginfo(f"[stopsign] my road: {self.coordinator.start_road}, other guy: {self.coordinator.other_croad}")
+        rospy.loginfo(f"[stopsign] Do I have prio? {self._is_to_the_right(me=self.coordinator.start_road, other=self.coordinator.other_croad)}")
+
+        return self._is_to_the_right(me=self.coordinator.start_road,
+                                     other=self.coordinator.other_croad)
--- src/coordination/src/coordination_strategies/strategy.py
+++ src/coordination/src/coordination_strategies/strategy.py
@@ -1,8 +1,6 @@
 import typing
 from abc import ABC, abstractmethod
 
-import rospy
-
 if typing.TYPE_CHECKING:
     from coordination.src.coordination_node import CoordinationNode
 
@@ -16,19 +14,6 @@ class CoordinationStrategy(ABC):
     def name(self) -> str:
         return self.name
 
-    def _is_to_the_right(self, me: str, other: str) -> bool:
-        mapping = {
-            'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'
-        }
-        return mapping[me] == other
-
     @abstractmethod
     def has_priority(self) -> bool:
-        assert self.coordinator.start_road is not None
-        assert self.coordinator.other_croad is not None
-
-        rospy.loginfo(f"[stopsign] my road: {self.coordinator.start_road}, other guy: {self.coordinator.other_croad}")
-        rospy.loginfo(f"[stopsign] Do I have prio? {self._is_to_the_right(me=self.coordinator.start_road, other=self.coordinator.other_croad)}")
-
-        return self._is_to_the_right(me=self.coordinator.start_road,
-                                     other=self.coordinator.other_croad)
+        ...
