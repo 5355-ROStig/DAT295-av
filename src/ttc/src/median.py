ec00d0f372a2e482ee2a88cf3908a16ae97fdeea
Martin H <marhilg@student.chalmers.se>
2021-12-13 17:38:27 +0100

    wip?

--- src/ttc/src/median.py
+++ src/ttc/src/median.py
@@ -1,3 +1,6 @@
+import math
+import statistics
+
 import numpy as np
 from filter import Filter
 import collections
@@ -10,19 +13,35 @@ class Median(Filter):
         self.buf.append((0, initPos))
 
     def _updateVel(self):
-        velSum = np.array([0, 0])
-        first = True
-        p = np.array([0, 0])
+        # x = statistics.median(p[1][0] for p in self.buf)
+        # y = statistics.median(y for _, (_, y) in self.buf)
+        # time_delta = statistics.median(p[0] for p in self.buf)
 
-        for val in self.buf:
-            if not first:
-                dt = val[0]
-                dp = val[1] - p
-                velSum = velSum + (dp / dt)
-            first = False
-            p = val[1]
+        velocities = []
 
-        self.v = velSum / self.buf.maxlen
+        for p1, p2 in zip(self.buf, self.buf[1:]):
+            displacement = p2[1] - p1[1]
+            time_delta = p2[0]
+            vel = displacement / time_delta
+            # velocities.append(math.sqrt(vel.dot(vel)))
+            velocities.append(vel)
+
+        med_vel = np.median(velocities, axis=0)
+        return *self.buf[-1][1], *med_vel
+
+        # velSum = np.array([0, 0])
+        # first = True
+        # p = np.array([0, 0])
+        #
+        # for val in self.buf:
+        #     if not first:
+        #         dt = val[0]
+        #         dp = val[1] - p
+        #         velSum = velSum + (dp / dt)
+        #     first = False
+        #     p = val[1]
+        #
+        # self.v = velSum / self.buf.maxlen
 
     def newData(self, dt, pos):
         # State transition matrix
