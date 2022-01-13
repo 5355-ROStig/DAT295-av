85abe6e109df6e507e3250a808f01ce16a6fc819
Martin H <marhilg@student.chalmers.se>
2022-01-13 14:52:14 +0100

    Leave phase: Keep driving a bit further than the target line

--- src/mission_planner/src/phases/leave_phase.py
+++ src/mission_planner/src/phases/leave_phase.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import Twist
 from phases.phase import Phase
 from std_msgs.msg import Empty
 
+
 class LeavePhase(Phase):
 
     def __init__(self, mission: "MissionPlannerNode"):
@@ -12,27 +13,35 @@ class LeavePhase(Phase):
         self.destination_road = self.mission.destination_road
         self.target_line = self.mission.stop_line
 
+        if self.destination_road.name == 'N':
+            self.condition_exp = lambda: self.mission.pos.y <= self.target_line - 500
+        elif self.destination_road.name == 'S':
+            self.condition_exp = lambda: self.mission.pos.y >= self.target_line + 500
+        elif self.destination_road.name == 'E':
+            self.condition_exp = lambda: self.mission.pos.x >= self.target_line + 500
+        elif self.destination_road.name == 'W':
+            self.condition_exp = lambda: self.mission.pos.x <= self.target_line - 500
+
     @property
     def name(self):
         return "Leave intersection"
 
     def begin(self):
+        self.mission.exit_pub.publish(Empty())
+        rospy.loginfo("I have left the intersection")
+
+    def run(self):
         twist = Twist()
-        twist.linear.x = 0
+        twist.linear.x = self.mission.drive_speed
         twist.linear.y = 0
         twist.linear.z = 0
         twist.angular.x = 0
         twist.angular.y = 0
         twist.angular.z = 0
         self.mission.cmd_vel_pub.publish(twist)
-        self.mission.exit_pub.publish(Empty())
-        rospy.loginfo("I have left!")
-
-    def run(self):
-        pass
 
     def finish(self):
-        pass
+        rospy.loginfo("Done!")
 
     def condition(self):
-        return True
+        return self.condition_exp()
